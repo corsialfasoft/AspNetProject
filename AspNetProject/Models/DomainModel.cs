1a3e3178cb32a71a6858c40ec8a068880ce13e0a
lucasalzone <luca.salzone@gmail.com>
2018-04-09 15:58:39 +0200

    assign issue

--- AspNetProject/Models/DomainModel.cs
+++ AspNetProject/Models/DomainModel.cs
@@ -6,11 +6,32 @@ using System.Web;
 namespace AspNetProject.Models {
     public class DomainModel {
         public interface IGestioneProdotti {
-            Prodotto CercabyCode(int CodiceProdotto);
-            List<Prodotto> CercabyDescrizione(string DescrizioneProdotto);
-            Prodotto AggiungiProdottoCarrello(int CodiceProdotto, int QuantitaRichiesta, int IdUtente);
-            void SvuotaCarello(int IdUtente);
+            Prodotto CercabyCode(int CodiceProdotto); //EDO
+            List<Prodotto> CercabyDescrizione(string DescrizioneProdotto); //LUCA
+            Prodotto AggiungiProdottoCarrello(int CodiceProdotto, int QuantitaRichiesta, int IdUtente); //ALESSIO
+            void SvuotaCarello(int IdUtente); //MATTEO
             void ConfermaOrdine(int IdUtente);
+        }
+        public class DomainLogistica : IGestioneProdotti {
+            public Prodotto AggiungiProdottoCarrello(int CodiceProdotto, int QuantitaRichiesta, int IdUtente) {
+                throw new NotImplementedException();
+            }
+
+            public Prodotto CercabyCode(int CodiceProdotto) {
+                throw new NotImplementedException();
+            }
+
+            public List<Prodotto> CercabyDescrizione(string DescrizioneProdotto) {
+                throw new NotImplementedException();
+            }
+
+            public void ConfermaOrdine(int IdUtente) {
+                throw new NotImplementedException();
+            }
+
+            public void SvuotaCarello(int IdUtente) {
+                throw new NotImplementedException();
+            }
         }
     }
 }
