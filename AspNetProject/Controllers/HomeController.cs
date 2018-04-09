05112807a4699a2943a88c30c8da9aecbf9079da
lucasalzone <luca.salzone@gmail.com>
2018-04-09 15:12:53 +0200

    add controller methods.

--- AspNetProject/Controllers/HomeController.cs
+++ AspNetProject/Controllers/HomeController.cs
@@ -6,9 +6,27 @@ using System.Web.Mvc;
 
 namespace AspNetProject.Controllers {
     public class HomeController : Controller {
+        public ActionResult RichiestaOrdine() {
+            return View();
+        }
+
+        public ActionResult DettaglioOrdine() {
+            return View();
+        }
+
+        public ActionResult ConfermaOrdine() {
+            return View();
+        }
+
+        public ActionResult carrello() {
+            return View();
+        }
+
+    
         public ActionResult Index() {
             return View();
         }
+
 
         public ActionResult About() {
             ViewBag.Message = "Your application description page.";
@@ -18,7 +36,6 @@ namespace AspNetProject.Controllers {
 
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
