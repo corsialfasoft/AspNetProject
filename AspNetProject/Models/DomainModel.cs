9d1e4219afc7192c0a242bb0de712e7bf5a4ebd3
JokerJackassJam <edo.bevione@gmail.com>
2018-04-09 15:15:54 +0200

    Creata classe Prodotto e Interfaccia nel DomainModel

---
+++ AspNetProject/Models/DomainModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetProject.Models {
+    public class DomainModel {
+        public interface IGestioneProdotti {
+            Prodotto CercabyCode(int CodiceProdotto);
+            List<Prodotto> CercabyDescrizione(string DescrizioneProdotto);
+            Prodotto AggiungiProdottoCarrello(int CodiceProdotto, int QuantitaRichiesta, int IdUtente);
+            void SvuotaCarello(int IdUtente);
+            void ConfermaOrdine(int IdUtente);
+        }
+    }
+}
---
+++ AspNetProject/Models/Prodotto.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetProject.Models {
+    public class Prodotto {
+        public string Descrizione {get; set;}
+        public int Giacenza {get; set;}
+        
+        public Prodotto (string descrizione, int giacenza) {
+            this.Descrizione=descrizione;
+            this.Giacenza=giacenza;
+        }
+    }
+}
